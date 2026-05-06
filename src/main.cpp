6db79167557e6e98573ef41fb72b480abe72782b
mork456 <alzawahramarkantony@gmail.com>
2026-05-06 13:15:44 -0400

    removed debug serial

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,6 @@ Servo rightServo;
 void setup () {
     // Start serial for HM-10 with baud rate 9600
     BLE.begin(9600);
-    // Serial for debugging arduino    
-    Serial.begin(9600);
 
     leftServo.attach(7);
     rightServo.attach(9);
@@ -37,7 +35,6 @@ void loop() {
             leftServo.write(leftSpeed);
             rightServo.write(rightSpeed);
             lastCommandTime = millis();
-            Serial.println(data);
         }
 
    }
